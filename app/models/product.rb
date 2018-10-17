367b89761474b7f091c4918c19e7996ad370ff2b
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-17 16:59:26 -0700

    add category id reference

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :orderproducts
+  belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :orders
+
+  resources :products do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :users, except: [:edit, :delete]
+
+  resources :categories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
