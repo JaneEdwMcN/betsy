88e0994e52b3a22bba83a2b9f3058aa52d4b5777
Maryam Shitu <maryamshitu@gmail.com>
2018-10-19 15:04:51 -0700

    mergeing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
+  before_action :find_user
 
   helper_method :logged_in?
   helper_method :current_user
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
