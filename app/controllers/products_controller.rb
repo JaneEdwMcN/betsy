a3f8c130502ff85bc82b14d17556edd54b77a8b5
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-20 16:11:46 -0700

    adjust before_action in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :find_product
+  before_action :find_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.order(:name)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def create
     auth_hash = request.env['omniauth.auth']
 
