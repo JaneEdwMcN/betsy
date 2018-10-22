b8ae4678508141f47c0c05d30270a02099d551f4
Divya <divyamohanan@gmail.com>
2018-10-22 12:53:36 -0700

    Added seed data for orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_201915) do
+ActiveRecord::Schema.define(version: 2018_10_22_180437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2018_10_21_201915) do
     t.datetime "updated_at", null: false
     t.integer "stock_count"
     t.integer "price"
-    t.string "category"
     t.string "photo_url"
     t.string "description"
     t.string "name"
