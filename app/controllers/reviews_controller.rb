5ea6479c8f8610f782136c9b49a74e1b333546ec
Jane <jane.edwards.mcnear@gmail.com>
2018-10-24 17:45:10 -0700

    creating validations for reviews and testing them

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :cant_leave_review_for_own_product, only: [:new, :create]
 
   def new
     @review = Review.new
@@ -11,9 +12,17 @@ class ReviewsController < ApplicationController
     redirect_to product_path(@review.product_id)
   end
 
-private
+  private
   def review_params
     params.require(:review).permit(:name, :rating, :review, :product_id)
+  end
+
+  def cant_leave_review_for_own_product
+    user = Product.find_by(id: params[:product_id]).user
+    if @current_user == user
+      flash[:warning] = "You cannot review your own products!"
+      redirect_to product_path(params[:product_id])
+    end
   end
 
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,9 +1,46 @@
 require "test_helper"
+require 'pry'
 
 describe Review do
-  let(:review) { Review.new }
+  describe "relationships" do
+    before do
+      @review = reviews(:one)
+      @lamb = products(:lamb)
+    end
 
-  it "must be valid" do
-    value(review).must_be :valid?
+    it "belongs to a product" do
+      @review.must_respond_to :product
+      @review.product.must_be_kind_of Product
+    end
+
+    it "has a product id" do
+      @review.product_id.must_equal @lamb.id
+    end
   end
+  name rating review product_id
+
+  describe "validations" do
+    before do
+      @review = reviews(:one)
+      @goat = products(:goat)
+    end
+
+    it "has a rating" do
+      review = Review.new(name: "Wendy", rating: 3, review: "Wonderful goat.", product_id: )
+      user.valid?.must_equal false
+    end
+
+    it "has a rating between 1 and 5" do
+    end
+
+    it "it's rating is a number" do
+    end
+
+    it "has a name" do
+    end
+
+    it "has a review" do
+    end
+  end
+
 end
