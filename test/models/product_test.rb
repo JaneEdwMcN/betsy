aadbb2ff620012fd9faad6c1d6ff82baef1ee9c4
Jane <jane.edwards.mcnear@gmail.com>
2018-10-25 18:59:23 -0700

    added simple cov to check test coverage

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,22 +2,22 @@ require "test_helper"
 
 describe Product do
 
-    it "has a list of categories" do
-      @product.categories << categories(:sleepy)
-      @product.must_respond_to :categories
+  it "has a list of categories" do
+    @product.categories << categories(:sleepy)
+    @product.must_respond_to :categories
 
-      @product.categories.each do |category|
-        category.must_be_kind_of Category
-      end
+    @product.categories.each do |category|
+      category.must_be_kind_of Category
     end
+  end
 
-    it "has a list of reviews" do
-      @product.must_respond_to :reviews
+  it "has a list of reviews" do
+    @product.must_respond_to :reviews
 
-      @product.reviews << reviews(:one)
-      @product.reviews.each do |review|
-        review.must_be_kind_of Review
+    @product.reviews << reviews(:one)
+    @product.reviews.each do |review|
+      review.must_be_kind_of Review
     end
-
-      #review cant belong to two items
+  end
+  #review cant belong to two items
 end
